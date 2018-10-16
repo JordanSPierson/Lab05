be428c06e5a6780eabd388180c3c0eb47c33a7cc
JordanSPierson <43154073+JordanSPierson@users.noreply.github.com>
2018-10-16 14:43:07 -0600

    More Comments

--- Lab5Driver.cpp
+++ Lab5Driver.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 #include "Lab5MyStack.h"
 
+//This main method tests the functions for each question
 
 int main()
 {
@@ -15,12 +16,16 @@ int main()
 	string eString,
 		nString;
 	cin >> eString;
+	//Test for [2] stringReversal1 function using a STL stack
 	nString = stringReversal1(eString);
 	cout << "\nOutput using an STL stack: " << nString << endl;
+	//Tests for [3] stringReversal2 funciton using a vector
 	nString = stringReversal2(eString);
 	cout << "Output using a vector: " << nString << endl;
+	//Tests for [4] stringReversal3 function using an STL list
 	nString = stringReversal3(eString);
 	cout << "Output using a list: " << nString << endl;
+	//Tests for [5] stringReversal5 using a stack class  
 	nString = stringReversal4(eString);
 	cout << "Outpout using a Stack class: " << nString << endl;
 
